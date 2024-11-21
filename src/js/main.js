8d1e0095a2c31912208595e094fe1f87c40d1852
21929457 <apple855005@gmail.com>
2024-11-21 14:02:02 +0900

    FITIN데이터 파일 추가

--- src/js/main.js
+++ src/js/main.js
@@ -5,5 +5,23 @@ let allTopTxt = $('.topTxt');
 let topTxt = active.find('.topTxt');
 
 setInterval(function(){
+    topTxt.removeClass('fadeUp')
     topTxt.addClass('fadeUp');
 } , 800)
+
+
+$.get('../../src/data/newProduct.json').then(function(data){
+    data.forEach((e , i) => {
+        $('.new .prod-wrap ul').append(
+           `<li>
+                <a href="">
+                    <div class="img-wrap"><img src="${e.url}"></div>
+                    <p class="product-title">${e.name}</p>
+                    <p class="product-price">${e.price}원</p>
+                </a>
+            </li>
+            ` 
+        )
+    });
+}).catch(function(){
+})
