e91f84f08fb048edafc81f2d48bc00a30b2037cc
21929457 <apple855005@gmail.com>
2024-11-21 14:30:27 +0900

    FITIN 이미지 추가

--- src/js/main.js
+++ src/js/main.js
@@ -25,3 +25,21 @@ $.get('../../src/data/newProduct.json').then(function(data){
     });
 }).catch(function(){
 })
+$.get('../../src/data/fitIn1.json').then(function(data){
+    data.forEach((e , i) => {
+        $('.fit-tab1').append(
+           `<li>
+                <a href="">
+                    <div class="img-wrap">
+                        <img src="${e.url}">
+                        <p class="product-code">${e.code}</p>
+                        <p class="product-title">${e.name}</p>
+                        <p class="product-detail-btn">View More</p>
+                    </div>
+                </a>
+            </li>
+            ` 
+        )
+    });
+}).catch(function(){
+})
